0858fe49a48d29c331a687bc94ab09caaef9f878
mmmGaya <gorbatovskaya-aya@yandex.ru>
2023-10-26 18:18:15 +0300

    add db reg

--- dialog_db/create_db.py
+++ dialog_db/create_db.py
@@ -25,8 +25,29 @@ def connect_to_database():
             person_name TEXT,
             person TEXT,
             assistant TEXT)''')
+    
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER,
+            user_name TEXT,
+            group_name TEXT)''')
     conn.commit()
    
+
+def register_user(user):
+    cur.execute('''INSERT INTO users (user_id, user_name, group_name) VALUES (%s, %s, %s)''', user)
+    conn.commit()
+
+def check_user(user_id):
+    cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id,)) 
+    user = cur.fetchone()
+    return user 
+
+def select_groups(group_name):
+    cur.execute("SELECT user_id FROM users WHERE group_name = %s", (group_name,)) 
+    users = cur.fetchone()
+    return users 
 
 async def insert_data(data):
     cur.execute('''INSERT INTO dialog (person_name, person, assistant) VALUES (%s, %s, %s)''', data)
